5e095898133b46ca2ddba72b7ae2de4dd5701d4d
Komal Band <komalband2006@gmail.com>
2026-04-07 23:48:54 +0530

    Time: 0 ms (100%), Space: 42.4 MB (10.31%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        // Base case
+        if (n == 1) return 0;
+
+        int mid = (int) Math.pow(2, n - 2);
+
+        if (k <= mid) {
+            return kthGrammar(n - 1, k);
+        } else {
+            return 1 - kthGrammar(n - 1, k - mid);
+        }
+    }
+}
